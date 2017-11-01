e9494ced4ca2eeec46fb1f8de6aa436214c3516d
polamjag <s@polamjag.info>
2017-11-01 18:41:35 +0900

    Initial Commit

---
+++ BroSwitch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BroSwitch
+//
+//  Created by ABE Satoru on 17-11-01.
+//  Copyright © 2017 polamjag. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BroSwitch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BroSwitch
+//
+//  Created by ABE Satoru on 17-11-01.
+//  Copyright © 2017 polamjag. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
